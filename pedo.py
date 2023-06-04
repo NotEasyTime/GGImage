8a8bed7bd3efc9311b2bb3a915fbe22e2eb522b7
Gabe <parry.gabe@icloud.com>
2023-06-04 13:23:45 -0400

    import file function

--- pedo.py
+++ pedo.py
@@ -1,4 +1,11 @@
 from tkinter import *
+from tkinter import filedialog
+
+def import_image():
+    global filename
+    filename = filedialog.askopenfilename ()
+    print("Selected: ", filename)
+
 
 root = Tk()
 
