9a5c371a0503699519c984c4ae91c38096e33a91
Gabe <parry.gabe@icloud.com>
2023-06-04 14:16:26 -0400

    message

--- pedo.py
+++ pedo.py
@@ -1,11 +1,6 @@
 from tkinter import *
 import tkinter
-
-
-
-
 from tkinter import filedialog
-import PIL
 from PIL import ImageTk, Image
 
 def import_image():
@@ -14,11 +9,15 @@ def import_image():
     print("Selected: ", filename)
     global image
     image = ImageTk.PhotoImage(Image.open(filename))
+    place_image()
 
+def place_image():
+    label1 = tkinter.Label(image=image)
+    label1.pack()
 
 root = tkinter.Tk()
+root.geometry('500x200')
 B = tkinter.Button(root, text = "Import Image", command = import_image)
-
 B.pack()
 root.mainloop()
 
