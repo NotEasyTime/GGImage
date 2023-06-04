6eff4b5a9a22ef5ff20d46022128afb7b3e5a96d
Gavin <gsmullen2005@gmail.com>
2023-06-04 17:05:56 -0400

    fix temp pack

--- pedo.py
+++ pedo.py
@@ -112,10 +112,9 @@ But = tkinter.Button(frame, text ="Hello", command = button_command)
 
 
 
+temp = tkinter.Button(frame, text = "Image Array", command = image_to_list)
+temp.pack_configure(padx = 0, pady = 0)
+
+
 root.geometry('500x300')
-
-
-temp = tkinter.Button(root, text = "Image Array", command = image_to_list)
-temp.pack()
-
 root.mainloop()
